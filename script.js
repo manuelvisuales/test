7a15cdba1ebd26ef651d56da6afa66c190a7a181
Manuel <120177542+manuelvisuales@users.noreply.github.com>
2023-08-26 00:24:30 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
-let lastScrollTop = 0;
-
-window.addEventListener("scroll", () => {
-  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  
-  if (scrollTop > lastScrollTop) {
-    document.querySelector(".background").style.backgroundPosition = "center " + -scrollTop / 2 + "px"; // Desplazamiento hacia arriba
-  } else {
-    document.querySelector(".background").style.backgroundPosition = "center " + scrollTop / 2 + "px"; // Desplazamiento hacia abajo
-  }
-  
-  lastScrollTop = scrollTop;
-});
-
 const galleryItems = document.querySelectorAll(".gallery-item");
-const lightbox = document.getElementById("lightbox");
-const lightboxImage = document.getElementById("lightbox-image");
-const closeButton = document.getElementById("close-button");
 
 galleryItems.forEach(item => {
   item.addEventListener("click", () => {
+    const lightboxImage = new Image();
     lightboxImage.src = item.src;
-    lightbox.style.display = "flex";
+    lightboxImage.alt = "Imagen en pantalla completa";
+    lightboxImage.classList.add("lightbox-image");
+
+    const lightbox = document.createElement("div");
+    lightbox.classList.add("lightbox");
+    lightbox.appendChild(lightboxImage);
+    
+    lightbox.addEventListener("click", () => {
+      lightbox.remove();
+    });
+
+    document.body.appendChild(lightbox);
   });
 });
 
-closeButton.addEventListener("click", () => {
-  lightbox.style.display = "none";
+window.addEventListener("scroll", () => {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  document.querySelector(".background").style.backgroundPositionY = -scrollTop / 2 + "px";
 });
